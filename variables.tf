c96bdb288c2f7cc31fb84940b99aff7962f338e4
Paul Boekschoten <paul.boekschoten@hashicorp.com>
2022-11-28 13:39:46 +0100

    security group and rules added

--- variables.tf
+++ variables.tf
@@ -12,3 +12,27 @@ variable "vpc_cidr" {
   type        = string
   description = "The IP range for the VPC in CIDR format."
 }
+
+variable "ssh_port" {
+  description = "Server port for SSH requests."
+  type        = number
+  default     = 22
+}
+
+variable "https_port" {
+  description = "Server port for HTTPS requests."
+  type        = number
+  default     = 443
+}
+
+variable "replicated_port" {
+  description = "Server port for Replicated dashboard."
+  type        = number
+  default     = 8800
+}
+
+variable "postgresql_port" {
+  description = "PostgreSQL database port."
+  type        = number
+  default     = 5432
+}
